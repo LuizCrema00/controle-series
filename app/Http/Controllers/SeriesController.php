3114ab3c02658eb196c5257af40d65f4f40830f7
Luiz Guilherme Crema <lgcrema@hotmail.com>
2023-07-28 12:27:44 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Middleware\Autenticador;
+use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\EloquentSeriesRepository;
+use App\Repositories\SeriesRepository;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function __construct(private SeriesRepository $repository) {
+        $this->middleware(Autenticador::class)->except('index');
+    }
+
+    public function index(Request $request)
+    {
+        
+        $series = Series::all();
+        $mensagemSucesso = session('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = $this->repository->add($request);
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function destroy(Series $series)
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\SeasonsController;
+use App\Http\Controllers\EpisodesController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Middleware\Autenticador;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::resource('series', SeriesController::class)
+    ->except(['show']);
+
+
+Route::middleware('autenticador')->group(function () {
+    Route::get('/', function () {
+            return redirect('/series');
+    });    
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])
+        ->name('seasons.index');
+    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('sigin');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+
+
+Route::get('/register', [UsersController::class, 'create'])->name('users.create');
+Route::post('/register', [UsersController::class, 'store'])->name('users.create');
